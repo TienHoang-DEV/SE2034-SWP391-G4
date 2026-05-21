873c31529e12a60cf7bbc3154be74561b4399e6a
tennguoidung9905-coder <tennguoidung9905@gmail.com>
2026-05-21 11:00:07 +0700

    third commit

---
+++ src/main/java/vn/edu/fpt/Se2034Swp391G4Application.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se2034Swp391G4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Se2034Swp391G4Application.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/fpt/Se2034Swp391G4ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.fpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se2034Swp391G4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
