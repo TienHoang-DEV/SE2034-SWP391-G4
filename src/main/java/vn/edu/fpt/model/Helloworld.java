9ddfd8cf5310db6120bcc28c32bb18b352c2defd
QuocAnh2005-cryto <quocanhtran952@gmail.com>
2026-05-21 12:31:01 +0700

    Hello chao duong

--- src/main/java/vn/edu/fpt/model/Helloworld.java
+++ src/main/java/vn/edu/fpt/model/Helloworld.java
@@ -2,6 +2,6 @@ package vn.edu.fpt.model;
 
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Toi la duong nhe!!!!");
     }
 }
