143d9cdaa2574095dfc162fdea82a8d1d7e5e7cb
Thanh Do <dothanh2572005@gmail.com>
2026-05-28 20:05:21 +0700

    add azure dependency and test .env connection

--- src/main/java/vn/edu/fpt/Se2034Swp391G4Application.java
+++ src/main/java/vn/edu/fpt/Se2034Swp391G4Application.java
@@ -1,5 +1,7 @@
 package vn.edu.fpt;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Se2034Swp391G4Application {
 
     public static void main(String[] args) {
+        // Load .env early so values are available as system properties and env for Spring
+        Dotenv dotenv = Dotenv.configure().filename(".env").ignoreIfMissing().load();
+        for (DotenvEntry entry : dotenv.entries()) {
+            // set as system property so Spring's ${...} can resolve it
+            System.setProperty(entry.getKey(), entry.getValue());
+        }
+
         SpringApplication.run(Se2034Swp391G4Application.class, args);
+
+        System.out.println(System.getProperty("AZURE_STORAGE_CONNECTION_STRING"));
     }
 
 }
--- src/main/java/vn/edu/fpt/controller/Helloworld.java
+++ src/main/java/vn/edu/fpt/controller/Helloworld.java
@@ -2,6 +2,6 @@ package vn.edu.fpt.controller;
 
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(System.getProperty("AZURE_STORAGE_CONNECTION_STRING"));
     }
 }
