732f57c5ffac0a61c83b128105470906e7d5c424
QuocAnh2005-cryto <quocanhtran952@gmail.com>
2026-06-01 17:24:23 +0700

    courseControllergit add .!

--- src/main/java/vn/edu/fpt/controller/CourseController.java
+++ src/main/java/vn/edu/fpt/controller/CourseController.java
@@ -1,14 +1,56 @@
 package vn.edu.fpt.controller;
 
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import vn.edu.fpt.entity.Category;
+import vn.edu.fpt.entity.User;
+import vn.edu.fpt.service.CategoryService;
+import vn.edu.fpt.service.UserService;
 
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
+@RequestMapping("/instructor")
 @Controller
 public class CourseController {
+     private UserService service;
+     private CategoryService categoryService;
+    public CourseController(UserService userService, CategoryService categoryService) {
+        this.service = userService;
+        this.categoryService = categoryService;
+    }
 
-    @GetMapping("/coursemanager")
-    public String getall(){
-        return "instructor_course/course_manager";
+    @GetMapping("/profile")
+    public String profile(HttpSession session, Model model){
+        //Sau có login
+//       User user = (User)session.getAttribute("user");
+
+       User tmp = service.findById(1);
+       model.addAttribute("instructor", tmp);
+       return "instructor_course/course_manager";
+    }
+
+    @PostMapping("/profiles")
+    public String updateProfile(
+                                @RequestParam("email") String email,
+                                @RequestParam("firstName") String firstName,
+                                @RequestParam("lastName") String lastName,
+                                @RequestParam("bio") String bio,
+                                @RequestParam("avatarFile") MultipartFile avatar,
+                                @RequestParam("phone") String phone,
+                                RedirectAttributes redirectAttributes
+                                ){
+        User tmp = service.findById(1);
+        service.updateProfileInstuctor(email, firstName, lastName, bio, phone, avatar);
+        redirectAttributes.addFlashAttribute("sucess","Thay đổi thành công!!!");
+        return "redirect:/instructor/profile";
     }
 }
---
+++ src/main/java/vn/edu/fpt/dto/InstructorDTO.java
@@ -0,0 +1,15 @@
+package vn.edu.fpt.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InstructorDTO {
+
+    private String name;
+    private String email;
+
+}
