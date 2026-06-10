c98b97f5f0e573e662aa20aeb91be24fd5f66f38
Nguyen Dang Hieu <nguyendanghieu130605@gmail.com>
2026-06-10 17:35:37 +0700

    "fix course manager"

--- src/main/java/vn/edu/fpt/repository/CourseRepository.java
+++ src/main/java/vn/edu/fpt/repository/CourseRepository.java
@@ -19,7 +19,7 @@ public interface CourseRepository extends JpaRepository<Course, Integer> {
     //Luu khoá học
     Course save(Course course);
     // Tìm danh sách các khóa học theo trạng thái
-    List<Course> findByStatus(String status);
+    List<Course> findByStatus(CourseStatus status);
 
 
     //Kiểm tra không title trùng
@@ -27,7 +27,7 @@ public interface CourseRepository extends JpaRepository<Course, Integer> {
 
 
 
-    long countByStatus(String status);
+    long countByStatus(CourseStatus status);
 
     // Tìm các khóa học có tên chứa từ khóa tìm kiếm (không phân biệt hoa thường)
     List<Course> findByTitleContainingIgnoreCase(String title);
@@ -46,7 +46,7 @@ public interface CourseRepository extends JpaRepository<Course, Integer> {
             """)
     Optional<Course> findByIdWithEnrollmentAndLessonProgress(@Param("id") Integer courseId);
 
-    List<Course> findByInstructorAndStatus(User user, String status);
+    List<Course> findByInstructorAndStatus(User user, CourseStatus status);
 
     @Query("""
             select c from Course c join c.enrollments e where e.user.id = :userId and c.id = :courseId
