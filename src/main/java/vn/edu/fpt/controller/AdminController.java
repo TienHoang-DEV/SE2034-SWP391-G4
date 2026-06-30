022c10e52bc9581d0938e8ad874cc9b41fb29c1f
Thanh Do <dothanh2572005@gmail.com>
2026-06-30 09:24:59 +0700

    fix admin controller

--- src/main/java/vn/edu/fpt/controller/AdminController.java
+++ src/main/java/vn/edu/fpt/controller/AdminController.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -33,6 +34,7 @@ public class AdminController {
      * GET /admin or GET /admin/dashboard
      * Hiển thị Dashboard tổng quan của Admin.
      */
+
     @GetMapping({"", "/dashboard"})
     public String dashboard(Model model) {
         long totalUsers = adminService.getTotalUsers();
