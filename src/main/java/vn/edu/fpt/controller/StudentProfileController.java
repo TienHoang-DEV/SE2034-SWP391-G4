5ab4a638349d023e0d3e18361285a71bdc96d988
tennguoidung9905-coder <tennguoidung9905@gmail.com>
2026-07-15 03:58:48 +0700

    fix bug profile

--- src/main/java/vn/edu/fpt/controller/StudentProfileController.java
+++ src/main/java/vn/edu/fpt/controller/StudentProfileController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import vn.edu.fpt.dto.user.StudentProfileDashboardDto;
 import vn.edu.fpt.entity.User;
 import vn.edu.fpt.service.StudentProfileService;
